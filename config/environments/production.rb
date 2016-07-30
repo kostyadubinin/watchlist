bdcc6e7faefe9d4e9d4f768015b4ffe1b5144fe5
Kostya Dubinin <dubininkostya@gmail.com>
2016-07-30 22:26:03 +0200

    Configure workers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 20).to_i
