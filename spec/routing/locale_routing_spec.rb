b5088cf1da66434be2b6efe0cb01d9bfd211dcb7
Kostya Dubinin <kostya.dubinin@gmail.com>
2016-08-15 22:21:55 +0200

    Hound suggestions

--- spec/routing/locale_routing_spec.rb
+++ spec/routing/locale_routing_spec.rb
@@ -6,17 +6,18 @@ RSpec.describe "routes with locale" do
   end
 
   it "routes / to movies index and does not set params[locale]" do
-    expect(get: "/")
-      .to route_to(controller: "movies", action: "index")
+    expect(get: "/").to route_to(controller: "movies", action: "index")
   end
 
   it "routes /en to movies index and sets params[locale] == :en" do
-    expect(get: "/en")
-      .to route_to(controller: "movies", action: "index", locale: "en")
+    expect(get: "/en").to(
+      route_to(controller: "movies", action: "index", locale: "en")
+    )
   end
 
   it "routes /nl to movies index and sets params[locale] == :nl" do
-    expect(get: "/nl")
-      .to route_to(controller: "movies", action: "index", locale: "nl")
+    expect(get: "/nl").to(
+      route_to(controller: "movies", action: "index", locale: "nl")
+    )
   end
 end
