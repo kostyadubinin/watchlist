873e54c6b15edf7c43af4b77ec22d2ab89fe3d68
Kostya Dubinin <kostya.dubinin@gmail.com>
2016-10-01 22:09:43 +0200

    Extract a movie backdrop url into the presenter (#75)
    * Extract a movie backdrop url into the presenter
    
    * HoundCI suggestions

--- app/presenters/movie_presenter.rb
+++ app/presenters/movie_presenter.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class MoviePresenter < BasePresenter
   alias movie model
+
+  def backdrop_url
+    "#{IMAGES_SECURE_BASE_URL}#{BACKDROP_SIZE}#{movie['backdrop_path']}"
+  end
 end
