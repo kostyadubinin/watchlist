44b74c476ef1b03bde49c5aa622c6cf9ff39f025
Kostya Dubinin <kostya.dubinin@gmail.com>
2016-10-02 08:35:09 +0200

    Extract movie vote average to the presenter (#77)

--- app/presenters/movie_presenter.rb
+++ app/presenters/movie_presenter.rb
@@ -5,4 +5,9 @@ class MoviePresenter < BasePresenter
   def backdrop_url
     "#{IMAGES_SECURE_BASE_URL}#{BACKDROP_SIZE}#{movie['backdrop_path']}"
   end
+
+  def vote_average
+    vote_average = movie["vote_average"]
+    h.number_with_precision(vote_average, precision: 2, significant: true)
+  end
 end
--- spec/presenters/movie_presenter.rb
+++ spec/presenters/movie_presenter.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe MoviePresenter do
-  subject(:presenter) { described_class.new("a", "b") }
+RSpec.describe MoviePresenter, type: :helper do
+  before { stub_movie_popular_request }
+
+  # TODO: Extract to a single place.
+  let(:api) { TmdbApi.new }
+
+  let(:movie) { api.movie_popular["results"].first }
+
+  subject(:presenter) { MoviePresenter.new(movie, helper) }
 
   it "aliases #movie to #model" do
     expect(presenter.movie).to equal(presenter.model)
   end
 
   describe "#backdrop_url" do
-    before { stub_movie_popular_request }
-
-    # TODO: Extract to a single place.
-    let(:api) { TmdbApi.new }
-
-    let(:movie) { api.movie_popular["results"].first }
-
-    subject(:presenter) { MoviePresenter.new(movie, _) }
-
     it "returns a backdrop url" do
       base_url = described_class::IMAGES_SECURE_BASE_URL
       backdrop_size = described_class::BACKDROP_SIZE
@@ -25,6 +23,12 @@ RSpec.describe MoviePresenter do
 
       expect(presenter.backdrop_url).
         to eq("#{base_url}#{backdrop_size}#{backdrop_path}")
+    end
+  end
+
+  describe "#vote_average" do
+    it "rounds vote average" do
+      expect(presenter.vote_average).to eq(6.0)
     end
   end
 end
