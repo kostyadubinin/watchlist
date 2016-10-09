64a09e426cc3a11f9d4b45acb6ba71df694d99c0
Kostya Dubinin <kostya.dubinin@gmail.com>
2016-10-09 09:20:54 +0200

    Redesign release date

--- app/presenters/movie_presenter.rb
+++ app/presenters/movie_presenter.rb
@@ -10,4 +10,18 @@ class MoviePresenter < BasePresenter
     vote_average = movie["vote_average"]
     h.number_with_precision(vote_average, precision: 2, significant: true)
   end
+
+  def release_date
+    h.time_ago_in_words(parsed_release_date) if parsed_release_date.present?
+  end
+
+  private
+
+  # TODO: Find a better solution.
+  def parsed_release_date
+    Date.parse(movie["release_date"])
+  rescue ArgumentError, TypeError => e
+    Raven.capture_exception(e)
+    nil
+  end
 end
--- spec/presenters/movie_presenter_spec.rb
+++ spec/presenters/movie_presenter_spec.rb
@@ -34,4 +34,30 @@ RSpec.describe MoviePresenter, type: :helper do
       expect(presenter.vote_average).to eq("5.9")
     end
   end
+
+  describe "#release_date" do
+    it "returns nil if the date is nil" do
+      movie = { "release_date" => nil }
+      presenter = MoviePresenter.new(movie, helper)
+
+      expect(presenter.release_date).to eq(nil)
+    end
+
+    it "returns nil if the date is an empty string" do
+      movie = { "release_date" => "" }
+      presenter = MoviePresenter.new(movie, helper)
+
+      expect(presenter.release_date).to eq(nil)
+    end
+
+    it "returns a distance of time in words to now" do
+      date = (Date.current - 17.months).to_s
+      movie = { "release_date" => date }
+
+      presenter = MoviePresenter.new(movie, helper)
+
+      expect(presenter.release_date)
+        .to eq(I18n.t("datetime.distance_in_words.over_x_years.one"))
+    end
+  end
 end
