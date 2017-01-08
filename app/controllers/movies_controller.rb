9d9340c275096dd74f91769464b48e1cfd0c5f15
Kostya Dubinin <kostya.dubinin@gmail.com>
2017-01-08 21:26:09 +0100

    Add pagination (#96)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 class MoviesController < ApplicationController
   def index
-    @movies = api.movie_popular["results"]
+    @movies = Kaminari.paginate_array(
+      movie_popular["results"],
+      total_count: movie_popular["total_results"]
+    ).page(params[:page]).per(20)
+  end
+
+  private
+
+  def movie_popular
+    @movie_popular ||= api.movie_popular(page: params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   filter :locale
 
   root "movies#index"
-  resources :movies, only: :index
+
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
+  resources :movies, only: :index, concerns: :paginatable
 end
